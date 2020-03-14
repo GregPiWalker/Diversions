83466ea7f884e87bdd0c1a2fce3663265d167ee7
Greg Walker <imalsoamember@yahoo.com>
2020-03-14 10:21:37 -0600

    Remove dead commented code.

--- MarshallingDelegation/MarshallingDelegate.cs
+++ MarshallingDelegation/MarshallingDelegate.cs
@@ -2,135 +2,17 @@
 using System;
 using System.Collections.Generic;
 using System.Reflection;
-using System.Threading;
 using System.Threading.Tasks;
 
 namespace MarshallingDelegation
 {
-    //TODO probably delete this interface
-    public interface IRedirect<TArg>
-    {
-        void Invoke(object sender, TArg arg);
-
-        Task InvokeAsync(object sender, TArg arg);
-    }
-
-    public class MarshallingDelegate<TArg> //: IRedirect<TArg>
+    public class MarshallingDelegate<TArg>
     {
         private static readonly ILog _Logger = LogManager.GetLogger(typeof(MarshallingDelegate<TArg>));
-        //private readonly List<MarshallingDelegate<TArg>> _invocationList = new List<MarshallingDelegate<TArg>>();
         private readonly List<DelegateBase<TArg>> _invocationList = new List<DelegateBase<TArg>>();
 
         public MarshallingDelegate()
         { }
-
-        //public MarshallingDelegate(Delegate temporary)
-        //    : this(temporary.Target, temporary.Method)
-        //{
-        //}
-
-        //public MarshallingDelegate(object target, MethodInfo method)
-        //{
-        //    DirectTarget = target;
-        //    DirectMethod = method;
-        //    DirectDelegate = (EventHandler<TArg>)Delegate.CreateDelegate(typeof(EventHandler<TArg>), target, method);
-        //}
-
-        //internal MarshalInfo MarshalInfo { get; set; }
-
-        //public EventHandler<TArg> DirectDelegate { get; private set; }
-        
-        ///// <summary>
-        ///// Gets the <see cref="MethodInfo"/> represented by the delegate.
-        ///// </summary>
-        ///// <exception cref="MemberAccessException">The caller does not have access to the method represented by the delegate</exception>
-        //public MethodInfo DirectMethod { get; private set; }
-        
-        ///// <summary>
-        ///// Gets the object on which the current delegate invokes the instance method, if the
-        ///// delegate represents an instance method; null if the delegate represents a static
-        ///// method.
-        ///// </summary>
-        //public object DirectTarget { get; private set; }
-
-        //public void Invoke(object sender, TArg arg)
-        //{
-        //    if (_invocationList.Count == 0)
-        //    {
-        //        var current = SynchronizationContext.Current;
-        //        if (MarshalInfo == null || MarshalInfo.MarshalMethod == null || MarshalInfo.Marshaller == null)
-        //        {
-        //            // Either there was no custom marshaller defined for the delegate target,
-        //            // or there is no delegate at all.
-
-        //            if (DirectDelegate == null)
-        //            {
-        //                // No event handlers are connected.
-        //                return;
-        //            }
-
-        //            DirectDelegate(sender, arg);
-        //        }
-        //        else if (MarshalInfo.MethodParameters.Length == 2)
-        //        {
-        //            if (SynchronizationContext.Current != null && SynchronizationContext.Current == MarshalInfo.SynchronizationContext)
-        //            {
-        //                if (DirectDelegate == null)
-        //                {
-        //                    // No event handlers are connected.
-        //                    return;
-        //                }
-
-        //                DirectDelegate(sender, arg);
-        //            }
-        //            else
-        //            {
-        //                // The defined marshaller's SyncrhonizationContext did not match the current context.
-        //                // A marshaller with parameters was defined, so inject the target action into the specified marshaller.
-        //                MarshalInfo.MarshalMethod.Invoke(MarshalInfo.Marshaller, new object[] { DirectDelegate, new object[] { sender, arg } });
-        //            }
-        //        }
-        //        else
-        //        {
-        //            // An invoker with static arguments was defined, so push the target action into the specified invoker
-        //            // using a lambda so that closure is performed over the arguments.
-        //            Action action = () => {
-        //                try
-        //                {
-        //                    DirectDelegate(sender, arg);
-        //                }
-        //                catch (Exception ex)
-        //                {
-        //                    _Logger.Error($"{nameof(DirectDelegate)}.{nameof(DirectMethod.Invoke)}: {ex.GetType().Name} during method invocation.", ex);
-        //                }
-        //            };
-
-        //            if (SynchronizationContext.Current != null && SynchronizationContext.Current == MarshalInfo.SynchronizationContext)
-        //            {
-        //                //TODO: Does this ever even happen?
-        //                action();
-        //            }
-        //            else
-        //            {
-        //                var indirectArgs = new object[] { action };
-        //                if (MarshalInfo.StaticArguments != null)
-        //                {
-        //                    indirectArgs = indirectArgs.Concat(MarshalInfo.StaticArguments).ToArray();
-        //                }
-
-        //                MarshalInfo.MarshalMethod.Invoke(MarshalInfo.Marshaller, indirectArgs);
-        //            }
-        //        }
-        //    }
-        //    else
-        //    {
-        //        foreach (var target in _invocationList)
-        //        {
-        //            target.Invoke(sender, arg);
-        //        }
-        //    }
-        //}
-
 
         public void Invoke(object sender, TArg arg)
         {
@@ -178,8 +60,6 @@ namespace MarshallingDelegation
 
         public DelegateBase<TArg> Add(Delegate toAdd)
         {
-            //var newDel = new MarshallingDelegate<TArg>(toAdd);
-
             DelegateBase<TArg> newDel = null;
             var attrib = toAdd.Method.GetCustomAttribute(typeof(ThreadedHandlerAttribute)) as ThreadedHandlerAttribute;
             if (attrib == null)
